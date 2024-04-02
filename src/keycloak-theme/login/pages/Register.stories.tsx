4df3565ae450f09b2bbf2dfea1c3f14fadf2699d
YusufZartit <gonenc@dakikyazilim.com>
2024-04-02 13:32:38 +0300

    Add Register.stories.tsx.

---
+++ src/keycloak-theme/login/pages/Register.stories.tsx
@@ -0,0 +1,22 @@
+import {Meta, StoryObj} from '@storybook/react';
+import {createPageStory} from "../createPageStory";
+
+const {PageStory} = createPageStory({
+    pageId: "register.ftl"
+});
+
+const meta = {
+    title: "login/Register",
+    component: PageStory,
+} satisfies Meta<typeof PageStory>;
+
+export default meta;
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+    render: () => <PageStory/>,
+};
+
+export const WithRegistrationEmailAsUsername: Story = {
+    render: () => <PageStory kcContext={{realm: {registrationEmailAsUsername: true}}}/>,
+};
